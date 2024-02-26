f298fba53b8aa941b013ae346a03f25f2c27d21b
Bruno Costa Ghisi <133908796+BrunoCGhisi@users.noreply.github.com>
2024-02-26 16:10:02 -0300

    Add files via upload

---
+++ ProjetoRevenda/127.0.0.1.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tempo de Geração: 26/02/2024 às 20h09min
+-- Versão do Servidor: 5.5.20
+-- Versão do PHP: 5.3.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Banco de Dados: `revenda`
+--
+CREATE DATABASE `revenda` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `revenda`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `marca`
+--
+
+CREATE TABLE IF NOT EXISTS `marca` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(5) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `marca`
+--
+
+INSERT INTO `marca` (`codigo`, `nome`) VALUES
+(1, 'fiat'),
+(2, 'Ford'),
+(11, 'teste');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `modelo`
+--
+
+CREATE TABLE IF NOT EXISTS `modelo` (
+  `codigo` int(5) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `codmarca` int(5) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `codmarca` (`codmarca`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `modelo`
+--
+
+INSERT INTO `modelo` (`codigo`, `nome`, `codmarca`) VALUES
+(1, 'Palio', 1),
+(2, 'Fiesta', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `veiculo`
+--
+
+CREATE TABLE IF NOT EXISTS `veiculo` (
+  `codigo` int(5) NOT NULL,
+  `descricao` varchar(50) NOT NULL,
+  `codmodelo` int(5) NOT NULL,
+  `ano` int(4) NOT NULL,
+  `cor` varchar(50) NOT NULL,
+  `placa` varchar(50) NOT NULL,
+  `opcionais` varchar(50) NOT NULL,
+  `valor` float(10,2) NOT NULL,
+  PRIMARY KEY (`codigo`),
+  KEY `codmodelo` (`codmodelo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `veiculo`
+--
+
+INSERT INTO `veiculo` (`codigo`, `descricao`, `codmodelo`, `ano`, `cor`, `placa`, `opcionais`, `valor`) VALUES
+(1, 'A feio bla bla bla', 1, 1980, 'Preto', '23F-BO1', 'Muito foda', 200000.00),
+(2, 'A feio bla bla bla', 2, 1985, 'Amarelo', 'XYZ123', 'vidro eletrico', 100000.00);
+
+--
+-- Restrições para as tabelas dumpadas
+--
+
+--
+-- Restrições para a tabela `modelo`
+--
+ALTER TABLE `modelo`
+  ADD CONSTRAINT `modelo_ibfk_1` FOREIGN KEY (`codmarca`) REFERENCES `marca` (`codigo`);
+
+--
+-- Restrições para a tabela `veiculo`
+--
+ALTER TABLE `veiculo`
+  ADD CONSTRAINT `veiculo_ibfk_1` FOREIGN KEY (`codmodelo`) REFERENCES `modelo` (`codigo`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
